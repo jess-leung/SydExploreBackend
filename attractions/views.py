2199bd762a7ca756b34b88f6beb93033ad69267a
Jessie Leung <jessieleung@Dumpling.local>
2014-09-13 14:24:38 +1000

    Classifier stuff

--- attractions/views.py
+++ attractions/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 import json
-import nltk 
 from django.utils import timezone
 from attractions.models import Review
 from attractions.models import Attraction
+import cPickle as pickle 
+from nltk import word_tokenize
+
+stopwords = [] 
+labels = ['Fun','Social','Adventurous','Lazy','Hungry','Natural','Cultural','Education','Historical','Luxurious']
 
 @csrf_exempt    
 def postReview(request):
@@ -14,17 +18,11 @@ def postReview(request):
     	# parse JSON 
     	# print request.body
         data = json.loads(request.body)
-        print data
         review_title=data['reviewTitle']
         review_text=data['reviewText']
         reviewer_name=data['reviewer']
         review_rating=data['rating']
         review_attraction=data['attraction']
-        print review_title
-        print review_text
-        print reviewer_name
-        print review_rating
-        print review_attraction
         # search what the attraction id is 
         attraction = Attraction.objects.get(name=review_attraction)
         # attraction_id = attraction.id 
@@ -33,9 +31,12 @@ def postReview(request):
         r = Review(review_title=review_title, reviewer_name=reviewer_name, review_text=review_text, review_rating=review_rating, attraction=attraction)
         r.save()
 
+        for line in open('textClassification/stopwords'):
+            stopwords.append(line.strip()) 
+
         # onto the machine learning bit 
-        review_category = classifyReview(review_text, review_title)
-        print review_category
+        review_category = classifyReview(review_attraction,review_text, review_title,labels,stopwords)
+        print 'This review is classified as: ',review_category
         # TODO
  		# get the category of the attraction that the review is affecting 
 
@@ -50,5 +51,48 @@ def postReview(request):
     
     return HttpResponse('Review Submitted')
 
-def classifyReview(review_text,review_title):
-	return 1
+def classifyReview(review_attraction,review_text,review_title,labels,stopwords):
+    review_text_tokenized = word_tokenize(review_text)
+    review_title_tokenized = word_tokenize(review_title)
+    review_attraction_tokenized = word_tokenize(review_attraction)
+    thisFeatures = getFeatures(review_attraction_tokenized,review_text_tokenized,review_title_tokenized,labels,stopwords)
+	classifierFile = open('textClassification/classifier.pkl','rb')
+    classifier = pickle.load(classifierFile)
+    this_class = classifier.predict(thisFeatures)
+    return this_class
+
+def getFeatures(attraction,title,bodyText,labels,stopwords): 
+    features = defaultdict() 
+    for word in attraction: 
+        features[('attraction_word',word)]=1
+    titleCount=0
+    for word in title: 
+        if word.lower() not in stopwords: 
+            features[('title_word',word.lower())]=1
+            if word.lower() in labels:
+                features[('title_label',word.lower())]=1
+            if titleCount==0:
+                features[('first_word',word)]=1
+        titleCount+=1
+    counter = 0
+    previousWord = ''
+    for word in bodyText: 
+        if word.lower() not in stopwords and word not in string.punctuation: 
+            features[('body_word',word)]=1
+            features[('body_word_stemmed',stem(word).lower())]=1
+            # if postags[counter][1]=='JJ':
+            #   features[('body_jj',word)]=1
+            # if postags[counter][1]=='NN':
+            #   features[('body_nn',word)]=1
+            if word.lower() in labels:
+                features[('body_label',word.lower())]=1
+            # if counter != 0:
+            #   features[('bigram',previousWord.lower()+word.lower())]=1
+            previousWord=word
+            # for key,value in gazeteer.iteritems():
+            #   if word.lower() in value: 
+            #       features[(key+'gazeteer',word.lower())]=1
+
+        counter+=1
+    features[('length_review',len(bodyText))]=1
+    return features
--- textClassification/classifyText.py
+++ textClassification/classifyText.py
@@ -9,37 +9,46 @@ from sklearn import metrics
 from porter2 import stem
 from sklearn.feature_selection import SelectKBest,chi2
 from sklearn.feature_extraction.text import TfidfVectorizer 
+import string 
+import operator
+import cPickle as pickle
+
 
 def getFeatures(attraction,title,bodyText,labels,stopwords): 
-	features = defaultdict() 
-	for word in attraction: 
-		features[('attraction_word',word)]=1
-	titleCount=0
-	for word in title: 
-		if word.lower() not in stopwords: 
-			features[('title_word',word.lower())]=1
-			if word.lower() in labels:
-				features[('title_label',word.lower())]=1
-			if titleCount==0:
-				features[('first_word',word)]=1
-		titleCount+=1
-	counter = 0
-	previousWord = ''
-	for word in bodyText: 
-		if word.lower() not in stopwords: 
-			features[('body_word',word)]=1
-			features[('body_word_stemmed',stem(word).lower())]=1
-			# if postags[counter][1]=='JJ':
-			# 	features[('body_jj',word)]=1
-			# if postags[counter][1]=='NN':
-			# 	features[('body_nn',word)]=1
-			if word.lower() in labels:
-				features[('body_label',word.lower())]=1
-			# if counter != 0:
-			# 	features[('bigram',previousWord.lower()+word.lower())]=1
-			previousWord=word
-		counter+=1
-	return features
+    features = defaultdict() 
+    for word in attraction: 
+        features[('attraction_word',word)]=1
+    titleCount=0
+    for word in title: 
+        if word.lower() not in stopwords: 
+            features[('title_word',word.lower())]=1
+            if word.lower() in labels:
+                features[('title_label',word.lower())]=1
+            if titleCount==0:
+                features[('first_word',word)]=1
+        titleCount+=1
+    counter = 0
+    previousWord = ''
+    for word in bodyText: 
+        if word.lower() not in stopwords and word not in string.punctuation: 
+            features[('body_word',word)]=1
+            features[('body_word_stemmed',stem(word).lower())]=1
+            # if postags[counter][1]=='JJ':
+            #   features[('body_jj',word)]=1
+            # if postags[counter][1]=='NN':
+            #   features[('body_nn',word)]=1
+            if word.lower() in labels:
+                features[('body_label',word.lower())]=1
+            # if counter != 0:
+            #   features[('bigram',previousWord.lower()+word.lower())]=1
+            previousWord=word
+            # for key,value in gazeteer.iteritems():
+            #   if word.lower() in value: 
+            #       features[(key+'gazeteer',word.lower())]=1
+
+        counter+=1
+    features[('length_review',len(bodyText))]=1
+    return features
 
 annotations_file = open('Annotations.tsv')
 annotations_dict = defaultdict(lambda: defaultdict())
@@ -48,29 +57,39 @@ true_labels = []
 counter=0
 labels = ['Fun','Social','Adventurous','Lazy','Hungry','Natural','Cultural','Education','Historical','Luxurious']
 stopwords = [] 
+gazeteer = defaultdict(lambda:defaultdict(int))
+gazeteer_sorted = defaultdict(list)
 # corpus=[]
 for line in open('stopwords'):
-	stopwords.append(line.strip())
+    stopwords.append(line.strip())
 
 for line in annotations_file:
-	if counter>0:
-		line = line.split('\t') 
-		annotations_dict[counter]['attraction']=word_tokenize(line[0])
-		annotations_dict[counter]['title']=word_tokenize(line[1])
-		tokenized_review = word_tokenize(line[2])
-		annotations_dict[counter]['review']=tokenized_review
-		annotations_dict[counter]['category']=line[3].strip()
-		# print line[2]
-		# annotations_dict[counter]['postag']=nltk.pos_tag(tokenized_review)
-		# print annotations_dict[counter]['postag']
-		# print 'here'
-		# corpus.append(tokenized_review)
-	counter+=1
-
+    if counter>0:
+        line = line.split('\t') 
+        annotations_dict[counter]['attraction']=word_tokenize(line[0])
+        annotations_dict[counter]['title']=word_tokenize(line[1])
+        tokenized_review = word_tokenize(line[2])
+        annotations_dict[counter]['review']=tokenized_review
+        annotations_dict[counter]['category']=line[3].strip()
+        # print line[2]
+        # annotations_dict[counter]['postag']=nltk.pos_tag(tokenized_review)
+        # print annotations_dict[counter]['postag']
+        # print 'here'
+        # corpus.append(tokenized_review)
+    counter+=1
 
 for key,value in annotations_dict.iteritems():
-	features_dict.append(getFeatures(value['attraction'],value['title'],value['review'],labels,stopwords))
-	true_labels.append(value['category'])
+    for word in value['review']:
+        if word.lower() not in stopwords and word not in string.punctuation: 
+            gazeteer[value['category']][word.lower()]+=1
+
+for key,value in gazeteer.iteritems():
+    gazeteer_sorted[key] = sorted(value.iteritems(), key=operator.itemgetter(1),reverse=True)
+    gazeteer_sorted[key] = [x[0] for x in gazeteer_sorted[key][:50]]
+    
+for key,value in annotations_dict.iteritems():
+    features_dict.append(getFeatures(value['attraction'],value['title'],value['review'],labels,stopwords))
+    true_labels.append(value['category'])
 
 clf = LinearSVC()
 vec = DictVectorizer()
@@ -82,44 +101,51 @@ selector = SelectKBest(chi2, k=450)
 true_labels_np = np.array(true_labels)
 features_dict_np = selector.fit_transform(features_dict_np, true_labels_np)
 
+save_object = raw_input('Press 1 for ten fold or 2 for saving classifier')
+save_object = int(save_object)
+if save_object == 1: 
+    # running list of test and true 
+    runningY_true = np.empty([2,2])
+    runningY_predicted = np.empty([2,2])
 
-# running list of test and true 
-runningY_true = np.empty([2,2])
-runningY_predicted = np.empty([2,2])
+    # 10 fold cross validation
+    skf = KFold(n=number_of_reviews, n_folds=10,shuffle=True)
+    count=0
+    for train, test in skf: 
+        print 'Testing fold',count
 
-# 10 fold cross validation
-skf = KFold(n=number_of_reviews, n_folds=10,shuffle=True)
-count=0
-for train, test in skf: 
-	print 'Testing fold',count
+        # getting the training and test sets for this fold 
+        X_train = features_dict_np[train]
+        y_train = true_labels_np[train]
+        X_test = features_dict_np[test]
+        y_test = true_labels_np[test]
 
-	# getting the training and test sets for this fold 
-	X_train = features_dict_np[train]
-	y_train = true_labels_np[train]
-	X_test = features_dict_np[test]
-	y_test = true_labels_np[test]
+        if count==0:
+            runningY_true=y_test
+        else:
+            # update runningY_true 
+            runningY_true=np.append(runningY_true,y_test)
+        # print runningY_true
+        # train classifier using training set
+        clf.fit(X_train,y_train)
 
-	if count==0:
-		runningY_true=y_test
-	else:
-		# update runningY_true 
-		runningY_true=np.append(runningY_true,y_test)
-	# print runningY_true
-	# train classifier using training set
-	clf.fit(X_train,y_train)
+        # classify test set
+        predictedClass = clf.predict(X_test)
 
-	# classify test set
-	predictedClass = clf.predict(X_test)
+        if count==0:
+            runningY_predicted=predictedClass
+        else: 
+            # update runningY_predicted 
+            runningY_predicted=np.append(runningY_predicted,predictedClass)
+        count+=1
 
-	if count==0:
-		runningY_predicted=predictedClass
-	else: 
-		# update runningY_predicted 
-		runningY_predicted=np.append(runningY_predicted,predictedClass)
-	count+=1
+    for y in runningY_true:
+        if y not in labels:
+            print y
 
-for y in runningY_true:
-	if y not in labels:
-		print y
-
-print metrics.classification_report(runningY_true, runningY_predicted,    target_names=labels)
+    print metrics.classification_report(runningY_true, runningY_predicted,    target_names=labels)
+else: 
+    pickleFile = open('classifier.pkl','wb')
+    clf.fit(features_dict_np,true_labels_np)
+    pickle.dump(clf, pickleFile, pickle.HIGHEST_PROTOCOL)
+    pickleFile.close()
