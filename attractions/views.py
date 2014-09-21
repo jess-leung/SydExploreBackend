22a47c53e7713af133e7bb6a017c307b724cf82c
Jessie Leung <jleu1656@gmail.com>
2014-09-21 15:13:52 +1000

    Changing getAttractions function to return location as well

--- attractions/views.py
+++ attractions/views.py
@@ -179,7 +179,7 @@ def getAttractions(request):
     try: 
         data = json.loads(request.body)
         category_key = labels_mapping[data['category_name']]
-        attractions = Attraction.objects.filter(category=category_key).values('name')
+        attractions = Attraction.objects.filter(category=category_key).values('name','location')
 
     except Exception,e:
         print 'Exception: Could not parse JSON ',str(e)
