f8bfc98075be8cd2fb168c6a0e0d6ce3b6acfe00
jsmi3195 <jess.dyer.smith@gmail.com>
2014-09-28 20:59:01 +1000

    trial

--- attractions/views.py
+++ attractions/views.py
@@ -194,6 +194,7 @@ def getReviewDetails(request):
         print 'TRIAL2'
         data=json.loads(request.body)
         print 'TRIAL3'
+        attraction_name = data['attraction_name']
         reviewDetails = Review.objects.filter(attraction=attraction_name)
         print 'TRIAL4'
         print reviewDetails
