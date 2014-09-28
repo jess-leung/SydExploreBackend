812cb175a89df86a4fc0b75372519b2a54c1053b
jsmi3195 <jess.dyer.smith@gmail.com>
2014-09-28 18:24:44 +1000

    Added in the url for getreviewdetails

--- attractions/urls.py
+++ attractions/urls.py
@@ -5,4 +5,5 @@ from attractions import views
 urlpatterns = [
     url(r'^postReview/$',views.postReview, name='postReview'),
     url(r'^getAttractions/$',views.getAttractions, name='getAttractions'),
+    url(r'^getReviewDetails/$',views.getReviewDetails,name='getReviewDetails'),
 ]
