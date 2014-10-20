367d2f0e4d0f8e2ee3b5ef1c093aa83f7dc5bf50
Jessie Leung <jleu1656@gmail.com>
2014-10-20 21:38:40 +1100

    Cleaning up code and adding comments

--- textClassification/classifyText.py
+++ textClassification/classifyText.py
@@ -13,15 +13,24 @@ import string
 import operator
 import cPickle as pickle
 
-
+''' Get features of the attraction '''
 def getFeatures(attraction,title,bodyText,labels,stopwords): 
+    # dictionary to hold features 
     features = defaultdict() 
+
+    # NOTE: if features are commented out, they were experimented with but deemed 
+    # as either unuseful or overfitting 
+
+    # loop through words in the attraction
     for word in attraction: 
         features[('attraction_word',word)]=1
     titleCount=0
+
+    # loop through words in title 
     for word in title: 
+        # check not in stopwords
         if word.lower() not in stopwords: 
-            # features[('title_word',word.lower())]=1
+            features[('title_word',word.lower())]=1
             if word.lower() in labels:
                 features[('title_label',word.lower())]=1
             if titleCount==0:
@@ -29,10 +38,14 @@ def getFeatures(attraction,title,bodyText,labels,stopwords):
         titleCount+=1
     counter = 0
     previousWord = ''
+
+    # lop through words in body text 
     for word in bodyText: 
+        # check not punctuation or stopword
         if word.lower() not in stopwords and word not in string.punctuation: 
             features[('body_word',word)]=1
             features[('body_word_stemmed',stem(word).lower())]=1
+            # unuseful features are commented out here 
             # if postags[counter][1]=='JJ':
             #   features[('body_jj',word)]=1
             # if postags[counter][1]=='NN':
@@ -50,7 +63,10 @@ def getFeatures(attraction,title,bodyText,labels,stopwords):
     features[('length_review',len(bodyText))]=1
     return features
 
+# open the annotations file 
 annotations_file = open('Annotations.tsv')
+
+# set up variables 
 annotations_dict = defaultdict(lambda: defaultdict())
 features_dict = []
 true_labels = [] 
@@ -59,41 +75,48 @@ labels = ['Fun','Social','Adventurous','Lazy','Hungry','Natural','Cultural','Edu
 stopwords = [] 
 gazeteer = defaultdict(lambda:defaultdict(int))
 gazeteer_sorted = defaultdict(list)
-# corpus=[]
+
+# get stopwords into list 
 for line in open('stopwords'):
     stopwords.append(line.strip())
 
+# loop through the annotations file 
 for line in annotations_file:
     if counter>0:
+        # tab delimited 
         line = line.split('\t') 
+
+        # tokenize the components of the review
         annotations_dict[counter]['attraction']=word_tokenize(line[0])
         annotations_dict[counter]['title']=word_tokenize(line[1])
         tokenized_review = word_tokenize(line[2])
         annotations_dict[counter]['review']=tokenized_review
         annotations_dict[counter]['category']=line[3].strip()
-        # print line[2]
-        # annotations_dict[counter]['postag']=nltk.pos_tag(tokenized_review)
-        # print annotations_dict[counter]['postag']
-        # print 'here'
-        # corpus.append(tokenized_review)
+        
     counter+=1
 
+# loop through annotations dictionary 
 for key,value in annotations_dict.iteritems():
+    # for review in all reviews
     for word in value['review']:
         if word.lower() not in stopwords and word not in string.punctuation: 
             gazeteer[value['category']][word.lower()]+=1
 
+# loop through gazeteer and put them into the gazeteer sorted 
 for key,value in gazeteer.iteritems():
     gazeteer_sorted[key] = sorted(value.iteritems(), key=operator.itemgetter(1),reverse=True)
     gazeteer_sorted[key] = [x[0] for x in gazeteer_sorted[key][:50]]
-    
+
+# loop through annotations dictionary and make feature vectors instances 
 for key,value in annotations_dict.iteritems():
     features_dict.append(getFeatures(value['attraction'],value['title'],value['review'],labels,stopwords))
     true_labels.append(value['category'])
 
+# create Linear SVC 
 clf = LinearSVC()
 vec = DictVectorizer()
-# vectorizer = TfidfVectorizer(min_df=1)
+
+# save the feature vectors
 number_of_reviews = len(true_labels)
 features_dict_np = vec.fit_transform(features_dict)
 pickleDictVecFile = open('dictvect.pkl','wb')
@@ -108,8 +131,11 @@ pickleSelectFile = open('selector.pkl','wb')
 pickle.dump(selector, pickleSelectFile, pickle.HIGHEST_PROTOCOL)
 pickleSelectFile.close()
 
+# User input - 1 for 10 fold cross validation or 2 for saving a classifier 
 save_object = raw_input('Press 1 for ten fold or 2 for saving classifier')
 save_object = int(save_object)
+
+# 10 fold 
 if save_object == 1: 
     # running list of test and true 
     runningY_true = np.empty([2,2])
@@ -150,7 +176,7 @@ if save_object == 1:
         if y not in labels:
             print y
 
-    print metrics.classification_report(runningY_true, runningY_predicted,    target_names=labels)
+    print metrics.classification_report(runningY_true, runningY_predicted, target_names=labels)
 else: 
     pickleFile = open('classifier.pkl','wb')
     clf.fit(features_dict_np,true_labels_np)
